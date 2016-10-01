10c8d7ccd03f0a4d1ea721e27fcdf8e9186b6f2e
kirillbukinevich <kirillbukinevich@gmail.com>
2016-10-01 11:36:52 +0300

    fix update attachment&phone fix pagination bar

---
+++ src/main/java/logic/database/PhoneDAO.java
@@ -0,0 +1,128 @@
+package logic.database;
+
+import logic.entity.ContactPhone;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static logic.configuration.LogConfiguration.LOGGER;
+
+/**
+ * Created by aefrd on 28.09.2016.
+ */
+public class PhoneDAO extends AbstractDAO{
+    public boolean addPhone(ContactPhone phone, final int EMPLOYEEID) {
+        updatePrepareStatement("INSERT INTO phone(code_country,code_operator,number,type,comment,employee_id) " +
+                "VALUES(?,?,?,?,?," + EMPLOYEEID + ")");
+        try {
+            this.preparedStatement.setInt(1, phone.getCodeCountry());
+            this.preparedStatement.setInt(2, phone.getCodeOperator());
+            this.preparedStatement.setInt(3, phone.getNumber());
+            this.preparedStatement.setString(4, phone.getType());
+            this.preparedStatement.setString(5, phone.getComment());
+            this.preparedStatement.executeUpdate();
+            LOGGER.info("add phone to BD");
+            phone.setId(retriveId(preparedStatement));
+        } catch (SQLException e) {
+            LOGGER.error("can't add phone to BD ", e);
+        }finally {
+            try {
+                if(preparedStatement!=null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error("can't close preparedStatement method  addphone to BD ", e);
+            }
+        }
+        return true;
+    }
+    public List<ContactPhone> getPhoneList(int ID) {
+        ArrayList<ContactPhone> phoneList = new ArrayList<>();
+        String query = "SELECT * FROM phone WHERE phone.employee_id=?";
+        try {
+            updatePrepareStatement(query);
+            preparedStatement.setInt(1,ID);
+            ResultSet e = this.preparedStatement.executeQuery();
+
+            while (e.next()) {
+                ContactPhone contactPhone = new ContactPhone();
+                contactPhone.setId(e.getInt(1));
+                contactPhone.setEmployeeID(e.getInt(2));
+                contactPhone.setCodeCountry(e.getInt(3));
+                contactPhone.setCodeOperator(e.getInt(4));
+                contactPhone.setNumber(e.getInt(5));
+                contactPhone.setType(e.getString(6));
+                contactPhone.setComment(e.getString(7));
+                phoneList.add(contactPhone);
+            }
+        } catch (SQLException var6) {
+            LOGGER.error("can't get phone list ", var6);
+        }
+
+        return phoneList;
+    }
+    public boolean updatePhone(ContactPhone phone){
+        String query = "UPDATE phone SET code_country=?,code_operator=?,number=?,type=?,comment=? " +
+                "WHERE id=?";
+        updatePrepareStatement(query);
+        try {
+            preparedStatement.setInt(1,phone.getCodeCountry());
+            preparedStatement.setInt(2,phone.getCodeOperator());
+            preparedStatement.setInt(3,phone.getNumber());
+            preparedStatement.setString(4, phone.getType());
+            preparedStatement.setString(5,phone.getComment());
+            preparedStatement.setInt(6,phone.getId());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            LOGGER.error("can't update phone to BD ", e);
+        }finally {
+            try {
+                if(preparedStatement!=null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error("can't close preparedStatement method  updatephone to BD ", e);
+            }
+        }
+        return true;
+    }
+
+    public boolean deletePhone(final int PHONEID) {
+        String deleteSQL = "DELETE FROM phone WHERE phone.id = ?";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL);
+            preparedStatement.setInt(1, PHONEID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException var5) {
+            LOGGER.error("can't delete phone ", var5);
+        }finally {
+            try {
+                if(preparedStatement!=null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return true;
+    }
+
+    public boolean deleteAllContactPhone(final int EMPLOYEEID) throws SQLException {
+        String deleteSQL = "DELETE FROM phone WHERE phone.employee_id = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL);
+        preparedStatement.setInt(1, EMPLOYEEID);
+        preparedStatement.executeUpdate();
+
+        return true;
+
+    }
+
+}
---
+++ src/main/java/logic/database/PhotoDAO.java
@@ -0,0 +1,79 @@
+package logic.database;
+
+import logic.entity.Photo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static logic.configuration.LogConfiguration.LOGGER;
+
+public class PhotoDAO extends AbstractDAO {
+    public Photo getPhoto(final int ID) {
+        String query = "select photo_name,employee_id from photo  WHERE photo.employee_id=?";
+        updatePrepareStatement(query);
+
+        Photo photo = new Photo();
+        try {
+            preparedStatement.setInt(1, ID);
+            ResultSet e = preparedStatement.executeQuery();
+            while (e.next()) {
+                String photoName = e.getString(1);
+                if (photoName != null) {
+                    photo.setPhotoName(photoName);
+                    photo.setExistInDB(true);
+                }
+                photo.setEmployeeID(e.getInt(2));
+            }
+        } catch (SQLException var5) {
+            LOGGER.error("can't get photo ", var5);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return photo;
+    }
+
+    public boolean deletePhoto(final int ID) throws SQLException {
+        String deleteSQL = "DELETE FROM photo WHERE photo.employee_id = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL);
+        preparedStatement.setInt(1, ID);
+        preparedStatement.executeUpdate();
+
+        return true;
+
+    }
+
+    public boolean updatePhoto(Photo photo) {
+        final int EMPLOYEEID = photo.getEmployeeID();
+        updatePrepareStatement("UPDATE photo SET photo_name=? WHERE employee_id=?");
+        try {
+            if (photo.isDeleted()) {
+                this.preparedStatement.setString(1, null);
+            } else {
+                this.preparedStatement.setString(1, photo.getPhotoName());
+            }
+            this.preparedStatement.setInt(2, EMPLOYEEID);
+            this.preparedStatement.executeUpdate();
+            LOGGER.info("update photo to BD");
+        } catch (SQLException e) {
+            LOGGER.error("can't update photo to BD ", e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+}
